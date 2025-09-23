08c36619ec8b3832a76afb9677dfcb6727641c89
Edgars G <Edgars@gmail>
2025-09-23 14:33:04 +0300

    game is finished with all extra bells and wisles

--- random.php
+++ random.php
@@ -4,42 +4,42 @@
 $gamecount = 0;
 $gameslost = 0;
 $points = 0;
+$pointsObtained = 0;
 
 while (true) {
-    $roundcount = 0;
     $gamespoints = 10;
     
     $rand = rand(1, 100);
     echo($rand . "\n");
-    while ($roundcount < 2) {
+    while (true) {
         $inter = readline("MINI CIPARU NO 1-100 --> ");
         if ($inter == $rand) {
-            echo("you guessed it right". $rand . "\n" );
+            echo("you guessed it right". "" . $rand . "\n" );
             $gamecount = $gamecount + 1;
             break;
         } elseif ($inter > $rand) {
             echo("you guessed a bit to large\n" );
-            $roundcount = $roundcount + 1;
             $gamespoints = $gamespoints - 1;
         } elseif ($inter < $rand) {
             echo("you guessed a bit to small\n" );
-            $roundcount = $roundcount + 1;
             $gamespoints = $gamespoints - 1;
-        } else {
-                echo("round lost \n");
-                $gameslost = $gameslost + 1;
-                $gamecount = $gamecount + 1;
-                break;
+        } 
+        
+        if ($gamespoints == 0) {
+            echo("round lost \n");
+            $gameslost = $gameslost + 1;
+            $gamecount = $gamecount + 1;
+            break;
             }
         }
-
-    $pointsObtained = $pointsObtained + $gamespoints - $gameslost; 
-    $ask = readline("vai vēlis doties?");
+            
+    $pointsObtained = $pointsObtained + $gamespoints; 
+    $ask = readline("vai vēlis doties? --> ");
 
     switch ($ask) {
         case 'ja':
-            $pointsObtained
-            echo("you won". $points . "this time\n");
+            echo("you won ". $pointsObtained . " point/s this time\n");
+            echo("you lost ". $gameslost . " this time\n");
             exit;
         case 'ne';
             break;
