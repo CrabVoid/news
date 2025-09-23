a6d15a88ad36c3edf26a1c822e7015c39b5df06e
Edgars G <Edgars@gmail>
2025-09-23 13:18:09 +0300

    game it self is working, but need to add multiple games where you can earn points

--- random.php
+++ random.php
@@ -1,10 +1,14 @@
 <?php
 
 $rand = rand(1, 100);
+echo($rand); #to see if you can actualy win
+
 
 while (true) {
-    $inter = readline("MINI CIPARU NO 1-100");
-    while (! rand == $inter) {
-        
+    $inter = readline("MINI CIPARU NO 1-100 --> ");
+    if ($inter == $rand) {
+        echo("you guessed it right". $rand . "\n" ); 
+    } else {
+        echo("try again \n");
     }
 }
