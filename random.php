3f22c415191218b9efa48b783a36a2749af01f04
Edgars G <Edgars@gmail>
2025-09-23 13:57:04 +0300

    you can earn points and go on basicaly for ever, it will repeat always

--- random.php
+++ random.php
@@ -1,37 +1,45 @@
 <?php
 
-$rand = rand(1, 100);
 
 $gamecount = 0;
 $gameswon = 0;
 $gameslost = 0;
 
-while ($gamecount < 10) {
+while (true) {
     $roundcount = 0;
+    $rand = rand(1, 100);
+    echo($rand . "\n");
     while ($roundcount < 10) {
-    $inter = readline("MINI CIPARU NO 1-100 --> ");
-    if ($inter == $rand) {
-        echo("you guessed it right". $rand . "\n" );
-        $gameswon = $gameswon + 1;
-        $gamescount = $gamescount + 1; 
-    } else {
-        if ($roundcount == 4) {
-            echo("round lost \n");
-            $gameslost = $gameslost + 1;
+        $inter = readline("MINI CIPARU NO 1-100 --> ");
+        if ($inter == $rand) {
+            echo("you guessed it right". $rand . "\n" );
+            $gameswon = $gameswon + 1;
             $gamecount = $gamecount + 1;
             break;
         } else {
-            echo("try again \n");
-            $roundcount = $roundcount + 1;
-        }
-    }
+            if ($roundcount == 4) {
+                echo("round lost \n");
+                $gameslost = $gameslost + 1;
+                $gamecount = $gamecount + 1;
+                break;
+            } else {
+                echo("try again \n");
+                $roundcount = $roundcount + 1;
+            }
+        }}
     $ask = readline("vai vēlis doties?");
-    if ($ask == "ja") {
-        echo ($gameswon . "" . "<--- uzvarēji tik spēles\n");
-        echo ($gameslost . "" . "<--- zaudēji tik spēles\n");
-        break;
+
+    switch ($ask) {
+        case 'ja':
+            echo ($gameswon . "" . "<--- uzvarēji tik spēles\n");
+            echo ($gameslost . "" . "<--- zaudēji tik spēles\n");
+            exit;
+        case 'ne';
+            break;
+        
+        default:
+            echo("what?");
     }
-    
-    
+
 }
-}
+
