517e77addbe1b43f190ea6c31abbb6f0db76ee04
Edgars G <Edgars@gmail>
2025-09-23 14:12:14 +0300

    you get 10 points for 1st try and for eavery other -1 form the max

--- random.php
+++ random.php
@@ -2,43 +2,51 @@
 
 
 $gamecount = 0;
-$gameswon = 0;
 $gameslost = 0;
+$points = 0;
 
 while (true) {
     $roundcount = 0;
+    $gamespoints = 10;
+    
     $rand = rand(1, 100);
     echo($rand . "\n");
-    while ($roundcount < 10) {
+    while ($roundcount < 2) {
         $inter = readline("MINI CIPARU NO 1-100 --> ");
         if ($inter == $rand) {
             echo("you guessed it right". $rand . "\n" );
-            $gameswon = $gameswon + 1;
             $gamecount = $gamecount + 1;
             break;
+        } elseif ($inter > $rand) {
+            echo("you guessed a bit to large\n" );
+            $roundcount = $roundcount + 1;
+            $gamespoints = $gamespoints - 1;
+        } elseif ($inter < $rand) {
+            echo("you guessed a bit to small\n" );
+            $roundcount = $roundcount + 1;
+            $gamespoints = $gamespoints - 1;
         } else {
-            if ($roundcount == 4) {
                 echo("round lost \n");
                 $gameslost = $gameslost + 1;
                 $gamecount = $gamecount + 1;
                 break;
-            } else {
-                echo("try again \n");
-                $roundcount = $roundcount + 1;
             }
-        }}
+        }
+
+    $pointsObtained = $pointsObtained + $gamespoints - $gameslost; 
     $ask = readline("vai vēlis doties?");
 
     switch ($ask) {
         case 'ja':
-            echo ($gameswon . "" . "<--- uzvarēji tik spēles\n");
-            echo ($gameslost . "" . "<--- zaudēji tik spēles\n");
+            $pointsObtained
+            echo("you won". $points . "this time\n");
             exit;
         case 'ne';
             break;
         
         default:
-            echo("what?");
+            echo("i guess you dont want to go  :)   \n");
+            break;
     }
 
 }
