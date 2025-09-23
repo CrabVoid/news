15534c25c56c7bd4acf4738de93a005d77d33b3a
Edgars G <Edgars@gmail>
2025-09-23 13:45:12 +0300

    added multiple games with win counters and loss counters

--- random.php
+++ random.php
@@ -1,14 +1,37 @@
 <?php
 
 $rand = rand(1, 100);
-echo($rand); #to see if you can actualy win
 
+$gamecount = 0;
+$gameswon = 0;
+$gameslost = 0;
 
-while (true) {
+while ($gamecount < 10) {
+    $roundcount = 0;
+    while ($roundcount < 10) {
     $inter = readline("MINI CIPARU NO 1-100 --> ");
     if ($inter == $rand) {
-        echo("you guessed it right". $rand . "\n" ); 
+        echo("you guessed it right". $rand . "\n" );
+        $gameswon = $gameswon + 1;
+        $gamescount = $gamescount + 1; 
     } else {
-        echo("try again \n");
+        if ($roundcount == 4) {
+            echo("round lost \n");
+            $gameslost = $gameslost + 1;
+            $gamecount = $gamecount + 1;
+            break;
+        } else {
+            echo("try again \n");
+            $roundcount = $roundcount + 1;
+        }
     }
+    $ask = readline("vai vēlis doties?");
+    if ($ask == "ja") {
+        echo ($gameswon . "" . "<--- uzvarēji tik spēles\n");
+        echo ($gameslost . "" . "<--- zaudēji tik spēles\n");
+        break;
+    }
+    
+    
+}
 }
