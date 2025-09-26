0c3fd9569af773129cb68a8a891bf17cb5c52517
Edgars G <Edgars@gmail>
2025-09-26 08:33:47 +0300

    all

--- array-exercises.php
+++ array-exercises.php
@@ -4,5 +4,17 @@
 
 #echo($fruit[2]);
 
-$country =["LV" => "Rīga", "D" => "Berlin", "EST" => "Tallin", "P" => "Lisbon", "F" => "Paris", "JP" => "Tokyo"];
-echo $country["JP"];
+#$country =["LV" => "Rīga", "D" => "Berlin", "EST" => "Tallin", "P" => "Lisbon", "F" => "Paris", "JP" => "Tokyo"];
+#echo $country["JP"];
+
+$cataloge = array (
+    array(212, "iRobots" , 3.20 , "Xiomi"),
+    array(321, "tējkanna" , 55.20 , "Bosch"),
+    array(303, "Dators" , 320 , "Dell"),
+);
+
+echo $cataloge[1][1];
+echo("\n");
+echo $cataloge[2][2]. ' ' .$cataloge[2][0];
+echo("\n");
+echo $cataloge[0][3];
